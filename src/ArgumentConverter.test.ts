4199f4113514ef1e709192bf77ad07622d343a2b
rs-amp <rsimpson@amplience.com>
2023-07-20 17:35:36 +0100

    test: add dummy tests so that CI can run

---
+++ src/ArgumentConverter.test.ts
@@ -0,0 +1,27 @@
+describe('fromGeneric', function() {
+    // Convert a minimal content item
+    // Convert all fields for classic
+    // Convert all fields for hotspots
+    // Convert all fields for gallery
+    // Convert all fields for mainAndDetail
+    // Convert all fields for moodboard
+    // Error when type is not recognized
+
+    test('Convert a minimal content item', () => {
+        // TODO
+    });
+});
+
+describe('fromSpecific', function() {
+    // Convert a minimal content item
+    // Convert all fields for classic
+    // Convert all fields for hotspots
+    // Convert all fields for gallery
+    // Convert all fields for mainAndDetail
+    // Convert all fields for moodboard
+    // Error when given type is not recognized
+
+    test('Convert a minimal content item', () => {
+        // TODO
+    });
+});
